0ee8f9126708d9cd9fa3fc682f48f85def1e6bbf
M_AK_NAHARI <125833989+Nahari9@users.noreply.github.com>
2023-04-18 05:29:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your Bot token in the quotes taken from @BotFather
-BOT_TOKEN = "1158822364:AAEgnhKPvoTbPU5KnO8iMYqQfuF_XcxbBUE"
+BOT_TOKEN = "5998037271:AAERa3tN_0xV-_pH32lzOFoipD2W4dJ0k1Y"
 
 # Project Name of your Project
 PROJECT_NAME = "GitGram"
 
 # If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
+GIT_REPO_URL = "https://github.com/Nahari9/GitGram"
